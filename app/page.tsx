3ab57b9ec19d38d22246332206cab65e8b751ade
Prasad Nikam <nikamprasad52@gmail.com>
2024-10-14 17:26:08 +0530

    testimonial, experience, Approach

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -11,6 +14,9 @@ export default function Home() {
 				<Hero />
 				<Grid />
 				<Projects />
+				<Clients />
+				<Experience />
+				<Approach />
 			</div>
 		</main>
 	);
